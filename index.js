9730bb346e3e60cd8f097b6d6ce9caebb971eb3b
GeruGames <49799704+gerudum@users.noreply.github.com>
2019-12-09 11:39:34 -0500

    Index.js base
    Bare minimum for the bot to run.

---
+++ index.js
@@ -0,0 +1,26 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+
+//Requires
+const Listener = require('./listener.js');
+
+const fs = require('fs');
+const prefix = "/";
+
+Listener.Listen();
+
+let data = JSON.parse(fs.readFileSync('.data/data.json','utf8'));
+
+bot.on('guildMemberAdd', member => {
+    member.guild.channels.get('channelID').send("Welcome " + member.displayName); 
+});
+
+bot.on('ready', () => {
+    console.log("Raring to go!");
+})
+
+bot.on('message', message=> {
+
+})
+
+bot.login(process.env.TOKEN);
