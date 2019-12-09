af913496c16ea408486548f41e87517c466c27a4
GeruGames <49799704+gerudum@users.noreply.github.com>
2019-12-09 11:45:34 -0500

    Command Collection

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
+
+
 const { prefix, token } = require('./config.json');
 client.commands = new Discord.Collection();
+
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	client.commands.set(command.name, command);
+}
+
 
 //Requires
 const Listener = require('./listener.js');
