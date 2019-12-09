8bb2388e74657b9449f9085ad47909a66559c8be
GeruGames <49799704+gerudum@users.noreply.github.com>
2019-12-09 11:50:55 -0500

    Dynamic Command Handling

--- index.js
+++ index.js
@@ -2,44 +2,52 @@ const Discord = require('discord.js');
 const bot = new Discord.Client();
 
 
-const { prefix, token } = require('./config.json');
-client.commands = new Discord.Collection();
+bot.commands = new Discord.Collection();
 
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
 for (const file of commandFiles) {
 	const command = require(`./commands/${file}`);
-	client.commands.set(command.name, command);
+	bot.commands.set(command.name, command);
 }
 
 
 //Requires
 const Listener = require('./listener.js');
 
+//FS is needed for saving/loading
 const fs = require('fs');
+
+//Prefix to all commands
 const prefix = "/";
 
 //Listen for pings.
 Listener.Listen();
 
+//Send this message when a new member joins
 bot.on('guildMemberAdd', member => {
     member.guild.channels.get('channelID').send("Welcome " + member.displayName); 
 });
 
+//Called when the bot gets online
 bot.on('ready', () => {
     console.log("Ready.");
 })
 
+//
 bot.on('message', message=> {
 	if (!message.content.startsWith(prefix) || message.author.bot) return;
 
 	const args = message.content.slice(prefix.length).split(/ +/);
 	const command = args.shift().toLowerCase();
 
-	if (command === 'ping') {
-		message.channel.send('Pong.');
-	} else if (command === 'beep') {
-		message.channel.send('Boop.');
+	if (!bot.commands.has(command)) return;
+
+	try {
+		bot.commands.get(command).execute(message, args);
+	} catch (error) {
+		console.error(error);
+		message.reply('there was an error trying to execute that command!');
 	}
 })
 
