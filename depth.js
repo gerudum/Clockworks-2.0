247c294da344206761697c131fc7eb5e5e417f46
GeruGames <49799704+gerudum@users.noreply.github.com>
2019-12-17 12:51:05 -0500

    Depth Direction

--- depth.js
+++ depth.js
@@ -1,7 +1,8 @@
 
 class Depth {
-  constructor(name,initLevelDate,initMarkerDate,levels,marker,rotationTime,markerRotationTime,nextRotation,nextMarkerRotation){
+  constructor(name,direction,initLevelDate,initMarkerDate,levels,marker,rotationTime,markerRotationTime,nextRotation,nextMarkerRotation){
       this.name = name; //Name of the depth
+      this.direction = direction; //Direction the levels will move
       this.initLevelDate = initLevelDate; //The date is was that position
       this.initMarkerDate = initMarkerDate; //The date the marker was at that position
       this.levels = levels; //The current levels
