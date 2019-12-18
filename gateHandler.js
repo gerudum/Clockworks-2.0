91ab522a2e4d2575c25c0e6ffee51a0eddfb254c
GeruGames <49799704+gerudum@users.noreply.github.com>
2019-12-18 10:45:35 -0500

    gateHandler Skeleton

---
+++ gateHandler.js
@@ -0,0 +1,12 @@
+//TODO: Data where all the depths are stored should be called here
+
+setInterval(function() {
+    Update();
+}, 5000);
+
+function Update(){
+  //TODO: Should check whether each depth should be ready to update
+
+}
+
+module.exports.Update = Update;
