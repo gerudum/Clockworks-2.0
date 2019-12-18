54ac1079336c7cf9cb0c04795b3ed97c37ad1e10
GeruGames <49799704+gerudum@users.noreply.github.com>
2019-12-18 10:38:37 -0500

    Comment Fix

--- depth.js
+++ depth.js
@@ -10,11 +10,11 @@ class Depth {
       this.levels = levels; //The current levels
       this.marker = marker; //The current marker
     
-      this.rotationTime = rotationTime; //How long it takes to rotate levels 
-      this.markerRotationTime = markerRotationTime; //How long it takes to rotate marker
+      this.rotationTime = rotationTime; //How long it takes to rotate levels  - Should be an array
+      this.markerRotationTime = markerRotationTime; //How long it takes to rotate marker - Should be an array
     
-      this.nextRotation = nextRotation; //Level Cycle Offset - Should be an array
-      this.nextMarkerRotation = nextMarkerRotation; //Marker Cycle oFfset - Should be an array
+      this.nextRotation = nextRotation; //Level Cycle Offset
+      this.nextMarkerRotation = nextMarkerRotation; //Marker Cycle oFfset
     
       this.rotationIndex = 0; //Where the level is currently on 
       this.markerRotationIndex = 0; //Where the marker is currently on
